ef1a6cc94ab5c12eca4d367ff1f4dba1cf0321b3
Brad Haas <bradh11@gmail.com>
2022-05-10 21:41:16 -0400

    better examples with tagfilters

--- example.py
+++ example.py
@@ -1,27 +1,120 @@
 from ciscoaxl import axl
 from lxml import etree
-cucm = '10.129.225.201'
-username = 'administrator'
-password = 'Pr3sid10!'
-version = '11.5'
-ucm = axl(username=username,password=password,cucm=cucm,cucm_version=version)
 
-# query = {"product": "Cisco Unified Client Services Framework"}
-# phones = ucm.get_phones(query=query)
-# for phone in phones:
-#     print(phone.name)
+cucm = "10.10.20.1"
+username = "administrator"
+password = "ciscopsdt"
+version = "12.5"
+ucm = axl(username=username, password=password, cucm=cucm, cucm_version=version)
 
-css = ucm.update_calling_search_space(
-name="WOB-HQ-CSS",
-addMembers=[
-    {
-        "member": {
-        "routePartitionName": "DUBLIN_PT",
-        "index": "0"
-        },
-        "member": {
-            "routePartitionName": "IPMA-PT",
-            "index": "1"
-        }
+# return a list of phones with the following fields populated
+phone_list = ucm.get_phones(
+    tagfilter={
+        "name": "",
+        "description": "",
+        "product": "",
+        "model": "",
+        "class": "",
+        "protocol": "",
+        "protocolSide": "",
+        "callingSearchSpaceName": "",
+        "devicePoolName": "",
+        "commonDeviceConfigName": "",
+        "commonPhoneConfigName": "",
+        "networkLocation": "",
+        "locationName": "",
+        "mediaResourceListName": "",
+        "networkHoldMohAudioSourceId": "",
+        "userHoldMohAudioSourceId": "",
+        "loadInformation": "",
+        "securityProfileName": "",
+        "sipProfileName": "",
+        "cgpnTransformationCssName": "",
+        "useDevicePoolCgpnTransformCss": "",
+        "numberOfButtons": "",
+        "phoneTemplateName": "",
+        "primaryPhoneName": "",
+        "loginUserId": "",
+        "defaultProfileName": "",
+        "enableExtensionMobility": "",
+        "currentProfileName": "",
+        "loginTime": "",
+        "loginDuration": "",
+        # "currentConfig": "",
+        "ownerUserName": "",
+        "subscribeCallingSearchSpaceName": "",
+        "rerouteCallingSearchSpaceName": "",
+        "allowCtiControlFlag": "",
+        "alwaysUsePrimeLine": "",
+        "alwaysUsePrimeLineForVoiceMessage": "",
     }
-])
+)
+
+for phone in phone_list:
+    print(phone.name)
+
+user_list = ucm.get_users(
+    tagfilter={
+        "userid": "",
+        "firstName": "",
+        "lastName": "",
+        "directoryUri": "",
+        "telephoneNumber": "",
+        "enableCti": "",
+        "mailid": "",
+        "primaryExtension": {"pattern": "", "routePartitionName": ""},
+        "enableMobility": "",
+        "homeCluster": "",
+        "associatedPc": "",
+        "enableEmcc": "",
+        "imAndPresenceEnable": "",
+        "serviceProfile": {"_value_1": ""},
+        "status": "",
+        "userLocale": "",
+        "title": "",
+        "subscribeCallingSearchSpaceName": "",
+    }
+)
+
+for user in user_list:
+    print(user.firstName, user.lastName, user.mailid, user.primaryExtension.pattern)
+
+hunt_pilots = ucm.get_hunt_pilots(
+    tagfilter={
+        "pattern": "",
+        "patternUrgency": "",
+        "routePartitionName": "",
+        "alertingName": "",
+        "blockEnable": "",
+        "description": "",
+        "forwardHuntBusy": "",
+        "forwardHuntNoAnswer": "",
+        "queueCalls": "",
+        "callingPartyTransformationMask": "",
+        "callingPartyPrefixDigits": "",
+        "callingLinePresentationBit": "",
+        "callingNamePresentationBit": "",
+        "calledPartyTransformationMask": "",
+        "callPickupGroupName": "",
+        "huntListName": "",
+        "useCallingPartyPhoneMask": "",
+        "maxHuntduration": "",
+        "displayConnectedNumber": "",
+        "aarNeighborhoodName": "",
+    }
+)
+
+for hunt_pilot in hunt_pilots:
+    print(hunt_pilot)
+
+hp = ucm.get_hunt_pilot(pattern="5600")
+print(hp)
+
+new_pilot = ucm.add_hunt_pilot(
+    pattern="2500",
+    description="new pilot",
+    blockEnable="false",
+    useCallingPartyPhoneMask="Off",
+    huntListName="VM_Hunt",
+)
+print(new_pilot)
