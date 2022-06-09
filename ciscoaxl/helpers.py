0e076e0a025fde4817322f58260f49041aa90237
arf <goobysc@gmail.com>
2022-06-09 11:44:18 -0400

    comments and cleanup

--- ciscoaxl/helpers.py
+++ ciscoaxl/helpers.py
@@ -1,15 +1,11 @@
 from functools import wraps, partial
-from typing import Callable, List, OrderedDict, TypeVar, Union, Sequence
+from typing import Callable, OrderedDict, TypeVar, Union, Sequence
 import inspect
-from zeep.helpers import serialize_object
 from zeep.xsd.valueobjects import CompoundValue
 from copy import deepcopy
-from ciscoaxl.exceptions import DumbProgrammerException
-from ciscoaxl.wsdl import (
-    get_return_tags,
-    fix_return_tags,
-)
-import ciscoaxl.configs as cfg
+from ciscoaxl.wsdl import fix_return_tags
+
+# import ciscoaxl.configs as cfg
 
 
 class _StaticIdentity:
@@ -33,21 +29,21 @@ def _tag_serialize_filter(tags: Union[list, dict], data: dict) -> dict:
     :return: Cleaned data with unwanted tags removed
     """
 
-    def check_value(d: dict) -> dict:
+    def check_for_value_1(d: dict) -> dict:
         d_copy = d.copy()
         for tag, value in d_copy.items():
             if type(value) == dict:
                 if "_value_1" in value:
                     d_copy[tag] = value["_value_1"]
                 else:
-                    d_copy[tag] = check_value(value)
+                    d_copy[tag] = check_for_value_1(value)
             elif type(value) == list:
                 for i, d in enumerate(deepcopy(value)):
                     if type(d) == dict:
-                        value[i] = check_value(d)
+                        value[i] = check_for_value_1(d)
                     elif hasattr(d, "__values__"):
                         d_odict = d.__values__
-                        d_filtered = check_value(dict(d_odict))
+                        d_filtered = check_for_value_1(dict(d_odict))
                         d.__values__ = OrderedDict(d_filtered)
                         value[i] = d
             elif hasattr(value, "__values__"):
@@ -55,7 +51,7 @@ def _tag_serialize_filter(tags: Union[list, dict], data: dict) -> dict:
                     d_copy[tag] = value["_value_1"]
                 else:
                     value_odict = value.__values__
-                    value_filtered = check_value(dict(value_odict))
+                    value_filtered = check_for_value_1(dict(value_odict))
                     value.__values__ = OrderedDict(value_filtered)
                     d_copy[tag] = value
         return d_copy
@@ -65,41 +61,34 @@ def _tag_serialize_filter(tags: Union[list, dict], data: dict) -> dict:
     data.pop("ctiid", None)
 
     if tags is None:
-        checked_data = check_value(data)
-        if not cfg.AUTO_INCLUDE_UUIDS:
-            checked_data.pop("uuid", None)
-        return checked_data
+        # no tag filtering, but clean up any '_value_1' issues
+        return check_for_value_1(data)
 
     working_data = deepcopy(data)
     for tag, value in data.items():
         if tag == "uuid":
             # * UUIDs are not included in wsdl descriptors, so they won't
-            # * show up automatically in 'tags'. We will always keep them in
-            # * unless AUTO_INCLUDE_UUIDS has been turned off. Then, we will
-            # * only keep UUIDs if 'uuid' is specified in return_tags
-            if cfg.AUTO_INCLUDE_UUIDS:
-                continue
-            elif "uuid" not in tags:
-                working_data.pop("uuid", None)
+            # * show up automatically in 'tags'. We will always keep them in.
+            continue
         elif tag not in tags and len(tags) > 0:
             working_data.pop(tag, None)
         elif type(value) == dict:
             if "_value_1" in value:
                 working_data[tag] = value["_value_1"]
             else:
-                working_data[tag] = check_value(value)
+                working_data[tag] = check_for_value_1(value)
         elif hasattr(value, "__values__"):
             if "_value_1" in value:
                 working_data[tag] = value["_value_1"]
             else:
                 value_odict = value.__values__
-                value_filtered = check_value(dict(value_odict))
+                value_filtered = check_for_value_1(dict(value_odict))
                 value.__values__ = OrderedDict(value_filtered)
                 working_data[tag] = value
         elif type(value) == list:
             for i, d in enumerate(deepcopy(value)):
                 if type(d) == dict:
-                    value[i] = check_value(d)
+                    value[i] = check_for_value_1(d)
     return working_data
 
 
@@ -116,93 +105,20 @@ def _tag_zeep_filter(tags: Union[list, dict], data: CompoundValue) -> CompoundVa
     return data
 
 
-"""Decorator that will process the func's `tagfilter` parameter
+"""@check_tagfilter(element_name, children [optional])
+Decorator that will process the func's `tagfilter` parameter
 and perform the following actions:
 
 - Check to see that all provided tags are valid 'returnedTags' base values
-- Convert `tagfilter`' list of base tag elements into a nested dict of all needed child tags
+- Convert `tagfilter`' list/dict of base tag elements into a nested dict of all necessary child tags
+- Filter out all attributes that are not tags included in 'tagfilter' (but always keep UUID if available)
 
 The `element_name` should be the name of the XSD element being called by the func.
+'children' should only be used in very specific situation where your 'returnedTags' needs to be
+    a subset of another element's returnedTags node. In that case, 'children' should either be a
+    string or a list/tuple of strings representing the children required to travel down to get to
+    the desired node.
 """
-
-
-def check_tags(element_name: str, children: Union[str, Sequence[str], None] = None):
-    def check_tags_decorator(func: TCallable) -> TCallable:
-        def processing(func, args, kwargs, children):
-            parameters = inspect.signature(func).parameters
-
-            if (filter_param := parameters.get("tagfilter", Missing)) is Missing:
-                raise DumbProgrammerException(
-                    f"No 'return_tags' param on {func.__name__}()"
-                )
-            # TODO: try to write non-kw-only version of this logic
-            if filter_param.kind != filter_param.KEYWORD_ONLY:
-                raise DumbProgrammerException(
-                    f"Forgot to add '*' before tagfilter on {func.__name__}()"
-                )
-            if not element_name:
-                raise DumbProgrammerException(
-                    f"Forgot to provide element_name in check_tags decorator on {func.__name__}!!!"
-                )
-            if children is not None:
-                if not isinstance(children, Sequence):
-                    raise DumbProgrammerException(
-                        "'children' should be either a str or Sequence[str] if not None"
-                    )
-                if type(children) == str:
-                    children = [children]
-
-            if "tagfilter" not in kwargs:
-                # tags are default
-                # if len(tags_param.default) == 0:
-                if filter_param.default is None:
-                    kwargs["tagfilter"] = fix_return_tags(
-                        z_client=args[0].zeep,
-                        element_name=element_name,
-                        tags=get_return_tags(args[0].zeep, element_name),
-                        children=children,
-                    )
-            elif type(kwargs["tagfilter"]) == list:
-                # supply all legal tags if an empty list is provided
-                if len(kwargs["tagfilter"]) == 0:
-                    kwargs["tagfilter"] = fix_return_tags(
-                        z_client=args[0].zeep,
-                        element_name=element_name,
-                        tags=get_return_tags(args[0].zeep, element_name),
-                        children=children,
-                    )
-                else:
-                    kwargs["tagfilter"] = fix_return_tags(
-                        z_client=args[0].zeep,
-                        element_name=element_name,
-                        tags=kwargs["tagfilter"],
-                        children=children,
-                    )
-
-        @wraps(func)
-        async def async_wrapper(*args, **kwargs):
-            # if cfg.DISABLE_CHECK_ARGS:
-            #     return await func(*args, **kwargs)
-            processing(func, args, kwargs, children)
-            return await func(*args, **kwargs)
-
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # if cfg.DISABLE_CHECK_TAGS:
-            #     return func(*args, **kwargs)
-            processing(func, args, kwargs, children)
-            return func(*args, **kwargs)
-
-        if inspect.iscoroutinefunction(func):
-            async_wrapper.element_name = element_name
-            async_wrapper.check = "tags"
-            return async_wrapper
-        else:
-            wrapper.element_name = element_name
-            wrapper.check = "tags"
-            return wrapper
-
-    return check_tags_decorator
 
 
 def check_tagfilter(element_name: str, children: Union[Sequence, None] = None):
@@ -213,7 +129,7 @@ def check_tagfilter(element_name: str, children: Union[Sequence, None] = None):
 
             if tag_param is Missing:
                 raise Exception(
-                    f"@check_tagfilter can't be used on {func.__name__}, has no 'tagfilter' parameter"
+                    f"@check_tagfilter cannot be used on {func.__name__}(), has no 'tagfilter' parameter"
                 )
 
             if type(children) == str:
@@ -233,10 +149,11 @@ def check_tagfilter(element_name: str, children: Union[Sequence, None] = None):
             elif (len(args) - 1) >= list(parameters).index("tagfilter"):
                 tag_arg_index = list(parameters).index("tagfilter")
                 full_tags = adjust_tags(args[tag_arg_index])
+                # func args are represented as a tuple, must replace immutable var
                 new_args = list(args)
                 new_args[tag_arg_index] = full_tags
                 args = tuple(new_args)
-            # no user-supplied value, use defined default
+            # no user-supplied value, there must be a default, use it
             else:
                 full_tags = adjust_tags(tag_param.default)
                 kwargs["tagfilter"] = full_tags
